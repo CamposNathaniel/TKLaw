b58bf26ff56bf409869ea5621b805e897fd24063
CamposNathaniel <blxrrgaming@gmail.com>
2018-02-25 23:41:22 -0600

    make button come up at 150px from top

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,7 +94,7 @@ $('#subscription-form').submit(function(e) {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
