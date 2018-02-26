1e49d131ccb82cb570f88f4c78c0d49bb3e1fd56
CamposNathaniel <blxrrgaming@gmail.com>
2018-02-25 23:43:35 -0600

    another go at fixing to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,7 +94,7 @@ $('#subscription-form').submit(function(e) {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
